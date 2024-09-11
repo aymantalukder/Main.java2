a5339b8d45f365cfdd4be54111635dcea42471cb
aymantalukder <aymant@nycstudents.net>
2024-09-11 00:25:56 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+public static void main(String[] args)
+    {
+
+//* primitives
+int i = 2;
+double b = 5.7;
+boolean c = false;
+//* reference
+String reference = "Hi there!";
+//*operator
+System.out.println(i+b);
+//*update value
+i=i-1;
+//*final variable
+final double pi =3.14;
+//* print
+System.out.print(i+b);
+System.out.print(c);
+System.out.print(reference);
+System.out.print(pi);
+    }
+
+                    }
